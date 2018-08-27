ba643dce490ab14a3f1addfe7d4027efe8fbc233
jack <993813173@qq.com>
2018-08-27 19:04:10 +0800

    8.27

--- OCPay_web_admin/src/App.vue
+++ OCPay_web_admin/src/App.vue
@@ -13,7 +13,6 @@ export default {
 	components: {
 	}
 }
-
 </script>
 
 <style lang="scss">
@@ -41,17 +40,14 @@ body {
 	vertical-align: baseline;
 	margin-right: 10px;
 }
-
 .toolbar {
 	background: #f2f2f2;
 	padding: 10px;
-	//border:1px solid #dfe6ec;
 	margin: 10px 0px;
 	.el-form-item {
 		margin-bottom: 10px;
 	}
 }
-
 .fade-enter-active,
 .fade-leave-active {
 	transition: all .2s ease;

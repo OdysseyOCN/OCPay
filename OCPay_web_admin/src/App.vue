66b67ab297e715407ef670838030a404b2455eba
jack <993813173@qq.com>
2018-08-27 19:05:17 +0800

    8.27

--- OCPay_web_admin/src/App.vue
+++ OCPay_web_admin/src/App.vue
@@ -23,19 +23,16 @@ body {
 	font-size: 14px;
 	-webkit-font-smoothing: antialiased;
 }
-
 #app {
 	position: absolute;
 	top: 0px;
 	bottom: 0px;
 	width: 100%;
 }
-
 .el-submenu [class^=fa] {
 	vertical-align: baseline;
 	margin-right: 10px;
 }
-
 .el-menu-item [class^=fa] {
 	vertical-align: baseline;
 	margin-right: 10px;

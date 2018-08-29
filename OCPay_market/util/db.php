6d610a17d098f0dfdbb1a20554751410e7dd0958
ricky <caopeng8787@163.com>
2018-08-29 15:18:35 +0800

    database optimization

---
+++ OCPay_market/util/db.php
@@ -0,0 +1,77 @@
+<?php
+
+// 数据库连接类
+class DB {
+    private static $dbcon = false;
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+    private $charset;
+    private $link;
+
+    private function __construct() {
+        $this->host = 'localhost';
+        $this->user = '';
+        $this->pass = '';
+        $this->db = '';
+        $this->charset = 'utf8';
+
+        // 连接数据库
+        $this->db_connect();
+
+        // 选择数据库
+        $this->db_usedb();
+        // 设置字符集
+        $this->db_charset();
+    }
+
+    // 连接数据库
+    private function db_connect() {
+        $this->link = mysqli_connect($this->host, $this->user, $this->pass);
+        if (!$this->link) {
+            echo "数据库连接失败<br/>";
+            echo "错误编码".mysqli_errno($this->link)."<br/>";
+            echo "错误信息".mysqli_error($this->link)."<br/>";
+            exit;
+        }
+    }
+
+    // 设置字符集
+    private function db_charset() {
+        mysqli_query($this->link, "set names {$this->charset}");
+    }
+
+    // 选择数据库
+    private function db_usedb() {
+        mysqli_query($this->link, "use {$this->db}");
+    }
+
+    // 私有的克隆
+    private function __clone() {
+        die("clone is not allowed");
+    }
+
+    // 公用的静态方法
+    public static function getIntance() {
+        if (self::$dbcon == false) {
+            self::$dbcon = new self;
+        }
+        return self::$dbcon;
+    }
+
+    /**
+     * 执行sql语句的方法
+     * @param $sql
+     * @return bool|mysqli_result
+     */
+    public function query($sql) {
+        $res = mysqli_query($this->link, $sql);
+        if (!$res) {
+            echo "sql语句执行失败<br>";
+            echo "错误编码是".mysqli_errno($this->link)."<br>";
+            echo "错误信息是".mysqli_error($this->link)."<br>";
+        }
+        return $res;
+    }
+}

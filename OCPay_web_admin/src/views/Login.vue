bcda3fe5d2c5c59d78f229766573463169a8a6a9
jack <993813173@qq.com>
2018-09-04 11:42:14 +0800

    9.4become

--- OCPay_web_admin/src/views/Login.vue
+++ OCPay_web_admin/src/views/Login.vue
@@ -10,7 +10,6 @@
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
       <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
   </el-form>
 </template>

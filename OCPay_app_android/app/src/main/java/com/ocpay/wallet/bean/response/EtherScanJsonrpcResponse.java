0c101365871b812190eeea9e6c7aaaa64fab51d3
jhon <jhonccy@gmail.com>
2018-09-20 10:24:20 +0800

    ocpay android. update code

---
+++ OCPay_app_android/app/src/main/java/com/ocpay/wallet/bean/response/EtherScanJsonrpcResponse.java
@@ -0,0 +1,35 @@
+package com.ocpay.wallet.bean.response;
+
+import com.snow.commonlibrary.utils.StringUtil;
+
+import java.math.BigInteger;
+
+import static com.ocpay.wallet.utils.CommonUtils.Hex2Decimal;
+
+public class EtherScanJsonrpcResponse {
+    public String jsonrpc;
+    public String result;
+    public String id;
+    public ErrorInfo error;
+
+
+    public BigInteger getDecimalFromDex() {
+        if (result.startsWith("0x")) {
+            return Hex2Decimal(result);
+        }
+        if (!StringUtil.isNumber(result) || StringUtil.isEmpty(result) || !result.startsWith("0x"))
+            return new BigInteger("0");
+        return new BigInteger(result);
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+
+
+
+
+
+
+}

09413093f9ed18180aad16afab43434167aac697
jhon <jhonccy@gmail.com>
2018-10-05 22:19:12 +0800

    ocpay android. update code

---
+++ OCPay_app_android/app/src/main/java/com/ocpay/wallet/bean/response/HomePageItem.java
@@ -0,0 +1,19 @@
+package com.ocpay.wallet.bean.response;
+
+import java.io.Serializable;
+
+public class HomePageItem implements Serializable {
+
+    public int id;
+    public int homePageId;
+    public int directType;
+    public String directUrl;
+    public int showSort;
+    public String showImg;
+    public String title;
+    public long createTime;
+    public int createBy;
+    public String updateTime;
+    public String updateBy;
+    public String content;
+}

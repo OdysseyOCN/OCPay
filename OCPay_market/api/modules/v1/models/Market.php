c8f11bb00fab90704d832dc89990ce811ee3bcfd
pengfen <caopeng8787@163.com>
2018-11-08 18:55:31 +0800

    optimization

--- OCPay_market/api/modules/v1/models/Market.php
+++ OCPay_market/api/modules/v1/models/Market.php
@@ -30,12 +30,25 @@ class Market extends ActiveRecord
 	/**
 	 * 获取当时时间的token信息
 	 *
-	 * @param $create_time 当前时间
+	 * @param $time 当前时间
 	 * @param $token
 	 * @return array 返回token信息
 	 */
-	public function get_list_for_token($create_time, $token) {
+	public function get_list_for_token($time, $token) {
 	    $sql = "select ID, exchange_name, token, currency, `close`, degree, vol from market where create_time = $time and token in ('{$token}') order by `close` limit 1";
+	    return Yii::$app->db->createCommand($sql)->queryOne();
+	}
+
+	/**
+	 * 获取当前时间的指定交易所的token信息
+	 * 
+	 * @param $time 当前时间
+	 * @param $token
+	 * @param $exchange 指定交易所
+	 * @return array 返回指定交易所的token信息
+	 */
+	public function get ($time, $token, $exchange) {
+		$sql = "select ID, exchange_name, token, currency, `close`, degree, vol from market where create_time = $time and token = '{$token}' and exchange_name = '{$exchange}' ";
 	    return Yii::$app->db->createCommand($sql)->queryOne();
 	}
 }

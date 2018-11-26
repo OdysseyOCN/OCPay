5cb07f008f12d84481c81d4fa3cf06933ca5a3d8
pengfen <caopeng8787@163.com>
2018-11-26 10:29:49 +0800

    optimization

--- OCPay_market/api/config/main.php
+++ OCPay_market/api/config/main.php
@@ -14,6 +14,9 @@ return [
         'v1' => [
             'class' => 'api\modules\v1\Module'
         ],
+        'v2' => [
+            'class' => 'api\modules\v2\Module'
+        ],
     ],
 
     //'controllerNamespace' => 'api\modules\v1\controllers',

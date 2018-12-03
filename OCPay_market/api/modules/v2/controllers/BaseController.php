2fb0a981ade790dfafb5d228a0c444ed49b88414
pengfen <caopeng8787@163.com>
2018-12-03 09:29:02 +0800

    optimization

--- OCPay_market/api/modules/v2/controllers/BaseController.php
+++ OCPay_market/api/modules/v2/controllers/BaseController.php
@@ -1,1 +1,33 @@
-BaseController.php
+<?php
+
+namespace api\modules\v2\controllers;
+
+use yii;
+use yii\rest\ActiveController;
+use yii\web\Response;
+use yii\filters\Cors;
+use yii\helpers\ArrayHelper;
+use api\modules\v2\models\Constant;
+use api\modules\v2\models\Log;
+
+class BaseController extends ActiveController
+{
+	public $modelClass = 'api\modules\v2\models\User';
+
+	public function behaviors()
+	{
+	    return ArrayHelper::merge([
+	        [
+	            'class' => Cors::className(),
+	            'cors' => [
+	                'Origin' => [Constant::cross_domain()],
+	                'Access-Control-Request-Headers' => [Constant::cross_domain()],
+	                'Access-Control-Request-Method' => ['GET', 'POST', 'OPTIONS'],
+	            ],
+	        ],
+	        'access' => [
+	    	    'class' => 'api\modules\v2\filters\AccessFilter',
+	    	],
+	    ], parent::behaviors());
+	}
+}

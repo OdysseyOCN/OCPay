281e28308f15cbddc0f917154ef8651ed374c21e
pengfen <caopeng8787@163.com>
2018-12-07 11:26:00 +0800

    optimization

--- OCPay_market/api/modules/v2/models/Collect.php
+++ OCPay_market/api/modules/v2/models/Collect.php
@@ -24,13 +24,12 @@ class Collect extends ActiveRecord
      * @param $plat_type   平台类型
     */
     public function get_col($exchange, $user_id, $curr_token, $plat_type) {
+        // 处理最优收藏
+        $sql = "select ID, type from collect where user_id = $user_id and token = '{$curr_token}' and plat_type = $plat_type ";
+
     	if ($exchange) { // 处理普通收藏
 			$sql = "select ID, type from collect where user_id = $user_id and token = '{$curr_token}' and exchange = '{$exchange}' and plat_type = $plat_type ";
-            $col_type = 2;
-		} else { // 处理最优收藏
-			$sql = "select ID, type from collect where user_id = $user_id and token = '{$curr_token}' and plat_type = $plat_type ";
-            $col_type = 1;
-		}
+		} 
 		return Yii::$app->db->createCommand($sql)->queryOne();
     }
 
@@ -45,14 +44,44 @@ class Collect extends ActiveRecord
     function add_collect($exchange, $user_id, $curr_token, $plat_type) {
         $col = self::get_col($exchange, $user_id, $curr_token, $plat_type);
 
+        $col_type = 1;
+        if ($exchange) {
+            $col_type = 2;
+        }
+
         if ($col) {
             $c_type = $col["type"];
             if ($c_type == 1 || $c_type == 2) $c_type = 0;
             else $c_type = 1;
 
             $id = $col["ID"];
-            Collect::updateAll(['type' => $c_type], 'ID = '.$id);
+            self::updateAll(['type' => $c_type], 'ID = '.$id);
+        } else {
+            self::add($user_id, $curr_token, $exchange, $col_type, $plat_type);
         }
         return '';
     }
+
+
+    /**
+     * 添加收藏
+     *
+     * @param $user_id 用户id
+     * @param $curr_token 当前币种
+     * @param $exchange 交易所
+     * @param $col_type 
+     * @param $plat_type 
+     */
+    public function add($user_id, $curr_token, $exchange, $col_type, $plat_type) {
+        $collect = new self();
+        $collect->user_id = $user_id;
+        $collect->token = $curr_token;
+        $collect->exchange = $exchange;
+        $collect->type = 1;
+        $collect->col_type = $col_type;
+        $collect->plat_type = $plat_type;
+        $collect->create_time = time();
+        $collect->save();
+    }
+
 }

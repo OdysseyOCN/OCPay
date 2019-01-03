44caefa6a377e67da0c62e7aa590f005aceec081
pengfen <caopeng8787@163.com>
2019-01-03 10:09:24 +0800

    optimization

---
+++ OCPay_market/api/modules/v2/models/HotSearch.php
@@ -0,0 +1,27 @@
+<?php
+namespace api\modules\v1\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+class HotSearch extends ActiveRecord
+{
+
+    /**
+     * @return string Active Record 类关联的数据库表名称
+     */
+    public static function tableName()
+    {
+        return '{{%hot_search}}';
+    }
+
+    /**
+     * 根据搜索词获取对应的列表
+     *
+     * @param $search 搜索词
+     */
+    public function get_list_for_search($search) {
+        $sql = "select token, search_count from hot_search where token = '{$search}' ";
+        return Yii::$app->db->createCommand($sql)->queryOne();
+    }
+}

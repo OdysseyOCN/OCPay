bcad0c42b2edd09bf6aa8005c0b7e1ea193ef022
pengfen <caopeng8787@163.com>
2019-02-20 18:59:18 +0800

    optimization

---
+++ OCPay_market/console/controller/FetchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace console\controllers;
+
+use yii\console\Controller;
+
+class FetchController extends Controller
+{
+    // 0 * * * * /usr/bin/php /var/www/ipfs/yii fetch/add
+    // 抓取数据
+    public function actionAdd() {
+
+    }
+}
